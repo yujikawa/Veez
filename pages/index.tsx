dc7cd2aaf433b6a0183fcd668cd3eeb47fe62682
yujikawa <ykyujikawa@gmail.com>
2022-12-02 08:31:30 +0900

    Add tauri

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { invoke } from '@tauri-apps/api/tauri'
+import { NextPage } from 'next'
+import { useEffect } from 'react';
+
+
 
 export default function Home() {
+  
+  invoke('greet', { name: 'World' })
+    .then(console.log)
+    .catch(console.error)
+
   return (
     <div className={styles.container}>
       <Head>
