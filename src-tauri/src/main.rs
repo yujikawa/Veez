1cedaec77194450f27b13f52e75f7278d040c931
yujikawa <ykyujikawa@gmail.com>
2022-12-02 21:49:01 +0900

    Add cmd

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,17 @@
     windows_subsystem = "windows"
 )]
 
+use std::fs;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Subrow Hello, {}!", name)
+fn cmd(input: &str) -> String {
+    // let paths = fs::read_dir(path).unwrap();
+    format!("Input command is {}!", input)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![cmd])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
